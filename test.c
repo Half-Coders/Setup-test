7c81fed5874b4911a189c6ece38b64d1858348fb
HabiburRahmanZihad <e241024@ugrad.iiuc.ac.bd>
2026-09-18 14:36:09 +0600

    setup code

---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    printf("Hello world! showing in text file");
+    return 0;
+   
+}
+
+
+// Ctrl+S = to save the file 
+// Ctrl+A = to select all the text in the file
+// Ctrl+C = to copy the selected text
+// Ctrl+V = to paste the copied text
